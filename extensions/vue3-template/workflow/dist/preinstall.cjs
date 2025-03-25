2242f1aa23d85bed26e4d747d3f9ebf59d7d052c
jerrychoux <jerrychoux@163.com>
2025-03-25 17:13:09 +0800

    ignore workflow/dist

--- extensions/vue3-template/workflow/dist/preinstall.cjs
+++
@@ -1,4 +0,0 @@
-"use strict";/yarn\.js$/.test(process.env.npm_execpath||"")||(console.error(`
-    \u26A0\uFE0F  This project requires Yarn for package management. 
-    Please use 'yarn install' instead of 'npm install'. 
-    `),process.exit(1));
